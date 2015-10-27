590e0441356c4e49f77f5e4567767af0056ce491
uyaer <uyaer@qq.com>
2015-10-27 17:22:33 +0800

    add html5 game page

---
+++ h5/js/main.js
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------------
+// ready
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+
+  $("#js-toggle-nav-menu").click(function () {
+    $(" #wdg_menu, .content, #wdg_footer_container_zibbo").toggleClass("flyout-active");
+  });
+
+});
