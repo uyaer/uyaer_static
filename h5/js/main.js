d20ecedfc18678324ea0506fae3e5e6b4f18f12e
uyaer <uyaer@qq.com>
2015-12-14 22:49:53 +0800

    apicloud

--- h5/js/main.js
+++ h5/js/main.js
@@ -7,4 +7,23 @@ $(document).ready(function () {
     $(" #wdg_menu, .content, #wdg_footer_container_zibbo").toggleClass("flyout-active");
   });
 
+  var now = Date.now();
+  var appId = "A6905655566893";
+  var appKey = "550A1EB9-EC16-2D80-D828-8BB8F9839EF6";
+  var appKey = SHA1(appId+"UZ"+appKey+"UZ"+now)+"."+now
+
+  var filter = {
+    fields:["gameId","name"]
+  }
+  $.ajax({
+    "url": "https://d.apicloud.com/mcm/api/list?filter=" + encodeURIComponent(JSON.stringify(filter)),
+    "method": "GET",
+    "cache": false,
+    "headers": {
+      "X-APICloud-AppId":appId,
+      "X-APICloud-AppKey": appKey
+    }
+  }).success(function (data, status, header) {
+
+  })
 });
